7c59d29567af11656b327b97f3a93fe0658885c1
ilyescmoi <ilyes.mechouar@epitech.eu>
2024-05-14 15:54:29 +0200

    feat: the project work properly

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-var submitButton = document.getElementById("submit");
-var img = document.getElementById("pokemon-image");
+const submitButton = document.getElementById("submit");
+const img = document.getElementById("pokemon-image");
+const map = new Map([["lixy", "assets/img/lixy.jpg"], ["pikachu", "assets/img/pikachu.jpg"]]);
+let actualPokemon = null;
 
 submitButton.addEventListener("click", function () {
-    var answer = document.getElementById("answer");
-    var message = document.getElementById("answer_message");
-    const map = new Map();
-    var randomNumber = Math.floor(Math.random() * map.size);
-    var randomPokemon = Array.from(map.keys())[randomNumber];
-    img.src = map.get(randomPokemon);
-    map.set("lixy", "assets/img/lixy.jpg");
-    map.set("pikachu", "assets/img/pikachu.jpg");
-    if (answer.value.toLowerCase() === randomPokemon || answer.value.toLowerCase() === "go") {
+    const answer = document.getElementById("answer");
+    const message = document.getElementById("answer_message");
+
+    if (answer.value.toLowerCase() === actualPokemon || answer.value.toLowerCase() === "go") {
         message.textContent = "Correct !";
-        randomNumber = Math.floor(Math.random() * map.size);
-        randomPokemon = Array.from(map.keys())[randomNumber];
+        const randomNumber = Math.floor(Math.random() * map.size);
+        const randomPokemon = Array.from(map.keys())[randomNumber];
+        actualPokemon = randomPokemon;
         img.src = map.get(randomPokemon);
     } else {
         message.textContent = "Incorrect !";
     }
 });
-
-
